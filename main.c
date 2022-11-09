f5bbf6ca90e8952257d65227f3084c42da063175
Nelman4528 <nelman4528@gmail.com>
2022-11-09 10:35:01 +0100

    Structure de Base Raylib

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include "map.h"
 
+
 void initialiserMap(Map map[45][30],HUD hud[6]){
 
     for (int i=0;i<45;i++){
         for (int j=0;j<30;j++){
-            map[i][j].etat=0;
+            map[i][j].etat=-1;
+            map[i][j].stade=0;
+            map[i][j].occupe=0;
         }
     }
     for (int i=0;i<6;i++){
@@ -12,137 +15,189 @@ void initialiserMap(Map map[45][30],HUD hud[6]){
     }
 }
 
-void sourisCase(Map map[45][30],HUD hud[6],int mouseX,int mouseY,int tailleW,int tailleH,int W,int H,ALLEGRO_COLOR white,ALLEGRO_FONT *gameFont,int clickGauche){
-    for (int i=0;i<6;i++){
-        if (mouseX>hud[i].x && mouseX<tailleW-hud[i].x && mouseY>hud[i].y && mouseY<tailleH+tailleH/1.5*(i+1)){
-            al_draw_filled_rectangle(hud[i].x,hud[i].y,tailleW-hud[i].x,tailleH+tailleH/1.5*(i+1),white);
-        }
+void remiseZero(HUD hud[6],int i){
+    for (int j=0;j<6;j++){
+        if (j!=i){
+        hud[j].etat=0;}
     }
-    for (int i=0;i<45;i++){
-        for (int j=0;j<30;j++){
-            al_draw_filled_rectangle(tailleW+W*i,tailleH+H*j,tailleW+W*(i+1),tailleH+H*(j+1), white);
+}
 
-            if (map[i][j].etat !=0){
-                al_draw_filled_rectangle(tailleW+W*i,tailleH+H*j,tailleW+W*(i+1),tailleH+H*(j+1), al_map_rgb(255,0,0));}
-            if (mouseX>tailleW+W*i && mouseX<tailleW+W*(i+1) && mouseY>tailleH+H*j && mouseY<tailleH+H*(j+1)){
-                al_draw_filled_rectangle(tailleW+W*i,tailleH+H*j,tailleW+W*(i+1),tailleH+H*(j+1),white);
-                al_draw_textf(gameFont,al_map_rgb(0, 0, 0) , tailleW + W / 2 + W * i, tailleH + H / 2 + H * j, 1, "%d", map[i][j].etat);
-                if(clickGauche == 1) {
-                    for (int k=0;k<6;k++){
-                        if (hud[k].etat==1){
-                            map[i][j].etat=k;
-                            hud[k].etat=0;
+
+void affichage(Map map[45][30],HUD hud[6])
+{
+    const int width = 800;
+    const int height = 450;
+    int tailleH=(height)/6;
+    int tailleW=(width)/9;
+    int H=(height-2*tailleH)/30; //Distance entre le centre de deux carré adjacent de la map
+    int W=(width-2*tailleW)/45; //Distance entre le centre de deux carré adjacent de la map
+
+    InitWindow(width, height, "ECE city");
+    float distance = 250.14f;
+    float angleVert = 45.0f;
+    float angleHorizon = 90.0f;
+    float distanceHorizon = distance * cosf(angleVert * PI / 180.0f);
+    float x = distanceHorizon* cosf(angleHorizon * PI / 180.0f);;
+    float y = distance * sinf(angleVert * PI / 180.0f);;
+    float z = distanceHorizon * sinf(angleHorizon * PI / 180.0f);
+    int collisionHUD;
+    int time;
+    int evolution=0; //Evoltion prochaine des maisons
+    int tempsBanni=0; //Temps deja utilsé pour l'evolution (pour eviter d'boluer plusieurs fois au meme moment)
+
+    Camera3D camera = { 0 };
+    camera.position = (Vector3){ x, y, z };
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+    camera.up = (Vector3){ 0.0f, 5.0f, 0.0f };
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    Vector3 cubePosition = { 0.0f, 0.0f, 0.0f }; // Dalle de la map
+    Vector3 cubeSize = { W, 0.0f, H };
+    Vector3 carte = { width/2, 0.0f, height/2 };
+    Vector3 carteSize = { width, 0.0f, height};
+
+    Ray ray = { 0 };
+    Vector2 mousePosition = {0};
+    Rectangle HUD = {10,50}; // Pour la collsion avec le HUD
+    HUD.height=50;
+    HUD.width=50;
+    RayCollision collision = { 0 };
+
+    SetCameraMode(camera, CAMERA_FREE);
+
+    SetTargetFPS(60);
+
+
+
+    while (!WindowShouldClose())
+    {
+
+        if (time%15==0 && time!=tempsBanni){ //Savoir si temps est un multipke de 15 pour l'evolution
+            evolution=1;
+            tempsBanni=time;
+        }
+        for (int i=0;i<45;i++){ // Evolution
+            for (int j=0;j<30;j++){
+                if (evolution==1 && map[i][j].stade<3 && map[i][j].etat==1){
+                    map[i][j].stade++;
+                    printf("%d\n",map[i][j].stade);
+                }
+
+            }
+        }
+        evolution=0;
+
+        UpdateCamera(&camera); // Système de camera de base de Raylib
+        ray = GetMouseRay(GetMousePosition(), camera);
+        mousePosition = GetMousePosition();
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+        if (IsKeyDown(KEY_LEFT)){ //Deplacement de la camera aussi possible avec ALt + mouseWheelPressed
+            x--;
+            camera.target = (Vector3){ x, y, z };
+        }
+        if (IsKeyDown(KEY_RIGHT)){
+            x++;
+            camera.target = (Vector3){ x, y, z };
+        }
+        if (IsKeyDown(KEY_UP)){
+            z--;
+            camera.target = (Vector3){ x, y, z };
+        }
+        if (IsKeyDown(KEY_DOWN)){
+            z++;
+            camera.target = (Vector3){ x, y, z };
+        }
+        BeginMode3D(camera);
+        for (int i=0;i<45;i++){
+            for(int j=0;j<30;j++){
+                cubePosition.x=i*W; //Coordonnée de la map
+                cubePosition.z=j*H;
+                //DrawCube(carte,carteSize.x,carteSize.y,carteSize.z,GRAY);
+
+
+                    DrawCube(cubePosition, W, 0.0f, H, GRAY);
+
+                collision = GetRayCollisionBox(ray,(BoundingBox){(Vector3){ cubePosition.x - cubeSize.x/2, 0, cubePosition.z - cubeSize.z/2 },
+                                                                 (Vector3){ cubePosition.x + cubeSize.x/2, 0, cubePosition.z + cubeSize.z/2 }});
+                if (collision.hit!=0){
+                    DrawCube(cubePosition,W,0.0f,H,RED);
+                    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
+                        for (int k=0;k<6;k++) {
+                            if (hud[k].etat == 1 && map[i][j].occupe==0 && map[i+2][j+2].occupe==0 && map[i][j+2].occupe==0 && map[i+2][j].occupe==0) {
+                                map[i][j].etat = k;
+                                map[i][j].occupe = 1;
+                                if (hud[1].etat==1){
+                                    for (int a=0;a<3;a++){
+                                        for (int b=0;b<3;b++){
+                                            map[i+a][j+b].etat = 1;
+                                            map[i+a][j+b].occupe = 1;
+                                        }
+                                    }
+                                }
+
+                            }
                         }
-
-
 
                     }
                 }
+                if (map[i][j].etat==0){  //Quoi faire en fonction de l'etat de la case (voir map.h)
+                    DrawCube(cubePosition,W,0.0f,H,BLACK);
+                }
+                else if (map[i][j].etat==1){ //Evolution des maisons
+                    if (map[i][j].stade==0){
+                        DrawCube(cubePosition,W*3/2,2.0f,H/1.2*3/2,RED);
+                    }
+                    if (map[i][j].stade==1){
+                        DrawCube(cubePosition,W/1.2*3/2,20.0f,H/1.2*3/2,GREEN);
+                    }
+                    if (map[i][j].stade>=2){
+                        DrawCube(cubePosition,W/1.2*3/2,200.0f,H/1.2*3/2,BLUE);
+                    }
+
+                }
+                else if (map[i][j].etat==2){
+                    DrawCube(cubePosition,W,0.0f,H,BLUE);
+                }
+                else if (map[i][j].etat==3){
+                    DrawCube(cubePosition,W,0.0f,H,GREEN);
+                }
+
             }
         }
+        EndMode3D();
+
+        for (int i=0;i<6;i++){ //Dessin du HUD
+            HUD.y=50+50*i;
+            DrawRectangleLines( HUD.x, HUD.y, HUD.width, HUD.height, BLUE);
+            collisionHUD = CheckCollisionPointRec(mousePosition,HUD);
+            if (collisionHUD != 0){
+                DrawRectangle( HUD.x, HUD.y, HUD.width, HUD.height, BLUE);
+                if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
+                    hud[i].etat=1;
+                    remiseZero(hud,i);
+
+                }
+            }
+        }
+        DrawRectangleLines( 10, 10, 100, 20, BLUE);
+        time = GetTime();
+        DrawText(TextFormat("Time %d", (time)), 20, 15, 10, BLACK);
+
+        EndDrawing();
+
     }
+
+    CloseWindow();
+
 
 }
 
-
-void Hud(HUD hud[6],int tailleW,int tailleH,int ClickGauche,int mouseX,int mouseY){
-
-    for (int i=0;i<6;i++){
-        hud[i].x=tailleW/6;
-        hud[i].y=tailleH+tailleH/1.5*i;
-        al_draw_rectangle(tailleW/6,tailleH+tailleH/1.5*i,tailleW-tailleW/6,tailleH+tailleH/1.5*(i+1), al_map_rgb(0,0,0),3);
-
-    }
-    if (ClickGauche==1){
-        for (int i=0;i<6;i++) {
-            if (mouseX > hud[i].x && mouseX < tailleW - hud[i].x && mouseY > hud[i].y &&
-                mouseY < tailleH + tailleH / 1.5 * (i + 1)) {
-                hud[i].etat = 1;
-            }
-        }
-    }
-}
-
-int main() {
-    ALLEGRO_DISPLAY* display = NULL;
-    ALLEGRO_MOUSE_STATE mouse_state;
-    ALLEGRO_TIMER *times;
+int main(){
     Map map[45][30];
     HUD hud[6];
-    srand(time(NULL));
-
-    ///INITIALISATION D'ALLEGRO
-    assert(al_init());
-    al_install_keyboard();
-    al_install_mouse();
-    al_init_image_addon();
-    al_init_font_addon();
-    al_init_ttf_addon();
-    al_init_primitives_addon();
-
-    al_set_new_display_flags(ALLEGRO_FULLSCREEN_WINDOW);
-    display = al_create_display(910, 610);
-    int height = al_get_display_height(display);
-    int width = al_get_display_width(display);
-    ALLEGRO_FONT *gameFont = al_load_ttf_font("/Users/nelkavakdjian/CLionProjects/untitled10/MagicCardsNormal.ttf", 30, ALLEGRO_ALIGN_LEFT);
-    int tailleH=(height)/6;
-    int tailleW=(width)/9;
-    int H=(height-2*tailleH)/30;
-    int W=(width-2*tailleW)/45;
-    float mouse_x = 0, mouse_y = 0;
-
-    ALLEGRO_COLOR black = al_map_rgb(0, 0, 0);
-    ALLEGRO_COLOR white = al_map_rgb(255, 255, 255);
-
-    times = al_create_timer(0.02);
-    int isEnd = 0;
-    ALLEGRO_EVENT_QUEUE* queue;
-    queue = al_create_event_queue();
-    assert(queue);
-    ALLEGRO_EVENT event = {0};
     initialiserMap(map,hud);
-    al_register_event_source(queue, al_get_display_event_source(display));
-    al_register_event_source(queue, al_get_keyboard_event_source());
-    al_register_event_source(queue, al_get_timer_event_source(times));
-    al_start_timer(times);
-    while (!isEnd) {
-        al_get_mouse_state(&mouse_state);
-        al_wait_for_event(queue, &event); // on pioche un événement dès qu'il y en a un
-        switch (event.type) { // en fonction de son type (événement de souris, du clavier...), on agit
-            case ALLEGRO_EVENT_DISPLAY_CLOSE:
-                isEnd = 1;
-                break;
-            case ALLEGRO_EVENT_KEY_DOWN:
-                switch (event.keyboard.keycode) {
-                    case ALLEGRO_KEY_ESCAPE:
-                        isEnd = 1;
-                        break;
-                }
-                break;
-
-        }
-        Hud(hud,tailleW,tailleH,mouse_state.buttons,mouse_state.x,mouse_state.y);
-        sourisCase(map,hud,mouse_state.x,mouse_state.y,tailleW,tailleH,W,H,white,gameFont,mouse_state.buttons);
-        for (int i=0;i<6;i++){
-            printf("%d\n",hud[i].etat);
-        }
-        //al_draw_rectangle(mouse_x-W/2,mouse_y-H/2,mouse_x+W/2,mouse_y+H/2,white,3);
-        for (int i = 0; i < 31; i++) {
-            al_draw_line(tailleW, tailleH + i * H, width - tailleW-36, tailleH + i * H, black, 1);
-        }
-        for (int i = 0; i < 46; i++) {
-            al_draw_line(tailleW + i * W, tailleH, tailleW + i * W, height - tailleH, black, 1);
-        }
-
-        al_flip_display();
-        al_clear_to_color(al_map_rgb(0,0,255));
-
-    }
-    al_destroy_event_queue(queue);
-
-
-
-
-
-    return 0;
+    affichage(map,hud);
 }
--- map.h
+++ map.h
@@ -3,25 +3,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_ttf.h>
-#include <allegro5/allegro_image.h>
-#include <allegro5/allegro_audio.h>
-#include <allegro5/allegro_acodec.h>
 #include <time.h>
+#include "raylib.h"
+#include <math.h>
 
 typedef struct {
-    double x,y;
-    int etat; //Construction present dessus (0 rien, 1 route, 2 centrale, 3 chateau d'eau, 4 terrain vague? )
-
+    int etat; //Construction present dessus (-1 rien, 0 route, 1 maison, 2 chateau d'eau, 3 centrale )
+    int stade;//Stade d'evolution de la maison (etat 1)
+    int occupe; //Savoir si il y a deja quelque chose sur la case
 }Map;
 
 typedef struct{
-    double x,y;
     int etat;
 
 }HUD;
+
 
 #endif
